94f3043b4ddb1c6d3076d7c05733487602bf833b
Ikenna Nwagbara <ikenna.nwagbara@gmail.com>
2020-12-09 20:20:10 -0500

    updated generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -7,7 +7,64 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
+function generatePassword() {
+  var generatePassword = "";
+
+  var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
+  var specialChar = "!@#$%^&*()_+{}|<>?:;~";
+  var numericChar = "1234567890";
+
+  var requiredCharString = "";
+
+  var lengthOfPassword = prompt(
+    "choose a length of at least 8 characters and no more than 128 characters"
+  );
+
+  if (lengthOfPassword < 8 || lengthOfPassword > 128) {
+    alert("Invalid character length");
+    lengthOfPassword;
+  }
+  console.log("password length" + lengthOfPassword);
+
+  var lowerCase = confirm(
+    "would you like lowercase characters in your password?"
+  );
+
+  if (lowerCase) {
+    requiredCharString = requiredCharString.concat(lowerCaseChar);
+  }
+
+  var upperCase = confirm(
+    "would you like uppercase characters in your password?"
+  );
+
+  if (upperCase) {
+    requiredCharString = requiredCharString.concat(upperCaseChar);
+  }
+
+  var special = confirm("would you like special characters in your password?");
+
+  if (special) {
+    requiredCharString = requiredCharString.concat(specialChar);
+  }
+
+  var numeric = confirm("would you like numbers in your password?");
+
+  if (numeric) {
+    requiredCharString = requiredCharString.concat(numericChar);
+  }
+
+  for (i = 0; i < lengthOfPassword; i++) {
+    var random = Math.floor(Math.random() * requiredCharString.length);
+
+    generatePassword = generatePassword.concat(requiredCharString.charAt(random));
+    console.log("Generate password is: " + generatePassword);
+  }
+
+  return generatePassword;
 }
 
 // Add event listener to generate button
